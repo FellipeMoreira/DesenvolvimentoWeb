12834d05a3d7d48515f13b33640480496a82d657
Matheus Moreira Rolim <fellipemr.key@gmail.com>
2022-12-12 21:49:04 -0300

    initial commit

---
+++ Trabalho4/etapas.js
@@ -0,0 +1,96 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '51222',
+                nome: 'Christianne Varão',
+                partido: 'PEN',
+                fotos: [
+                    {url: 'cv1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '55555',
+                nome: 'Homero do Zé Filho',
+                partido: 'PSL',
+                fotos: [
+                    {url: 'cv2.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '43333',
+                nome: 'Dandor',
+                partido: 'PV',
+                fotos: [
+                    {url: 'cv3.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '15123',
+                nome: 'Filho',
+                partido: 'MDB',
+                fotos: [
+                    {url: 'cv4.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Chiquinho do Adbon',
+                partido: 'PDT',
+                vice: 'Arão',
+                fotos: [
+                    {url: 'cp3.jpg', legenda: 'Prefeito'},
+                    {url: 'v3.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '15',
+                nome: 'Malrinete Gralhada',
+                partido: 'MDB',
+                vice: 'Biga',
+                fotos: [
+                    {url: 'cp2.jpg', legenda: 'Prefeito'},
+                    {url: 'v2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '45',
+                nome: 'Dr. Francisco',
+                partido: 'PSC',
+                vice: 'João Rodrigues',
+                fotos: [
+                    {url: 'cp1.jpg', legenda: 'Prefeito'},
+                    {url: 'v1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '54',
+                nome: 'Zé Lopes',
+                partido: 'PPL',
+                vice: 'Francisca Ferreira Ramos',
+                fotos: [
+                    {url: 'cp4.jpg', legenda: 'Prefeito'},
+                    {url: 'v4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '65',
+                nome: 'Lindomar Pescador',
+                partido: 'PC do B',
+                vice: 'Malú',
+                fotos: [
+                    {url: 'cp5.jpg', legenda: 'Prefeito'},
+                    {url: 'v5.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            }
+        ]
+    }
+]
